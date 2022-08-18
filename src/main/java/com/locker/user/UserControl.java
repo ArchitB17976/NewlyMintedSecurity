a5406207654df813d570862d8962b41e22f5f09a
ArchitB17976 <44702605+ArchitB17976@users.noreply.github.com>
2022-08-18 17:03:41 +0800

    defined controller class

--- src/main/java/com/locker/user/UserControl.java
+++ src/main/java/com/locker/user/UserControl.java
@@ -1,5 +1,32 @@
 package com.locker.user;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
 public class UserControl 
-{    
+{
+    private final List<User> USERS = Arrays.asList(
+        new User(120000, "Derek Schneider"),
+        new User(120001, "Brie Ulrich"),
+        new User(120002, "Kirk Lee")
+    );
+
+    @GetMapping(path = "{user_id}")
+    public User getUser(@PathVariable("user_id") Long user_id)
+    {
+        return USERS.stream()
+                        .filter(user -> user_id.equals(user.getId()))
+                        .findFirst()
+                        .orElseThrow(() -> 
+                            new IllegalStateException(
+                                "User " + user_id + " does not exist."
+                            ));
+    }
 }

9c93ef2885c2534dcb167e049f193ee916b70fda
ArchitB17976 <44702605+ArchitB17976@users.noreply.github.com>
2022-08-18 17:03:25 +0800

    defined User class

--- src/main/java/com/locker/user/User.java
+++ src/main/java/com/locker/user/User.java
@@ -1,5 +1,25 @@
 package com.locker.user;
 
-public class User {
+public class User 
+{
+    private final long id;
+    private final String name;
     
+    public long getId() 
+    {
+        return id;
+    }
+    
+    public String getName() 
+    {
+        return name;
+    }
+
+    public User(long id, String name) 
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+
 }

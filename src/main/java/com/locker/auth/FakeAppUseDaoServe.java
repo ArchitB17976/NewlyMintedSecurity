5483ee3765cfe2cad05e503af4ce964059a50dc2
ArchitB17976 <44702605+ArchitB17976@users.noreply.github.com>
2022-08-24 02:51:14 +0800

    Completing fake

--- src/main/java/com/locker/auth/FakeAppUseDaoServe.java
+++ src/main/java/com/locker/auth/FakeAppUseDaoServe.java
@@ -1,37 +1,62 @@
 package com.locker.auth;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
 
-import com.locker.security.AppUserRole.*;
+import com.google.common.collect.Lists;
 
+import static com.locker.security.AppUserRole.*;
+
+@Repository("Fake") // Name used in case of multiple repo implementations
 public class FakeAppUseDaoServe implements AppUserDAO
 {
     private final PasswordEncoder encoding;
 
     @Autowired
-    public FakeAppUseDaoServe(PasswordEncoder encoder)
-    {
-        this.encoding = encoder;
-    }
+    public FakeAppUseDaoServe(PasswordEncoder encode){ this.encoding = encode; }
 
     @Override
     public Optional<AppUser> selectAppUserByUsername(String username) 
     {
-        return Optional.empty();
+        return getAppUsers()
+                .stream()
+                .filter(appUser -> username.equals(appUser.getUsername()))
+                .findFirst();
     }
 
     private List<AppUser> getAppUsers()
     {
-        List<AppUser> appUsers = List.new ArrayList(
+        return Lists.newArrayList(
             new AppUser(
-                ADMIN.getGrantedAuths(), 
+                NORMAL_USER.getGrantedAuths(),
+                "brie",
+                encoding.encode("pass"),
+                true,
+                true,
+                true,
+                true
+            ),
+            new AppUser(
+                ADMIN.getGrantedAuths(),
                 "kirk",
-                encoding.encode("pass123")
+                encoding.encode("pass123"),
+                true,
+                true,
+                true,
+                true
+            ),
+            new AppUser(
+                ADMIN_TRAINEE.getGrantedAuths(),
+                "derek",
+                encoding.encode("pass456"),
+                true,
+                true,
+                true,
+                true
             )
-        )
+        );
     }
 }

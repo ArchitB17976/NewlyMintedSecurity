d6fe5645f4e0fc6c1df652d82321ee4f76670840
ArchitB17976 <44702605+ArchitB17976@users.noreply.github.com>
2022-08-24 02:09:07 +0800

    Creating and implementing DAO into service class

--- src/main/java/com/locker/auth/AppUserServe.java
+++ src/main/java/com/locker/auth/AppUserServe.java
@@ -6,10 +6,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class AppUserServe implements UserDetailsService
 {
+    private final AppUserDAO dataObj;
+
+    public AppUserServe(AppUserDAO appDAO)
+    {
+        this.dataObj = appDAO;
+    }
+    
     @Override
     public UserDetails loadUserByUsername(String username) 
         throws UsernameNotFoundException 
     {
-        return null;
+        return dataObj
+                    .selectAppUserByUsername(username)
+                    .orElseThrow(() -> new UsernameNotFoundException(
+                        String.format("Username %s not found.", username)
+                    ));
     }
 }

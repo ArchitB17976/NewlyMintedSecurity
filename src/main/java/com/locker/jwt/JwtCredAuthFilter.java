c2d738b0340fccb07383a2c41199124995681a8a
ArchitB17976 <44702605+ArchitB17976@users.noreply.github.com>
2022-08-24 17:30:39 +0800

    Overriding successfulAuth() to send JWT Tokens

--- src/main/java/com/locker/jwt/JwtCredAuthFilter.java
+++ src/main/java/com/locker/jwt/JwtCredAuthFilter.java
@@ -1,12 +1,19 @@
 package com.locker.jwt;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
 import org.springframework.security.authentication.*;
 import org.springframework.security.core.*;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.*;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -39,5 +46,30 @@ public class JwtCredAuthFilter extends UsernamePasswordAuthenticationFilter
             return manage.authenticate(authentication);
         }
         catch(IOException e) { throw new RuntimeException(e); }
+    }
+
+    @Override
+    protected void successfulAuthentication(
+        HttpServletRequest request, 
+        HttpServletResponse response, 
+        FilterChain chain,
+        Authentication authResult
+    ) throws IOException, ServletException 
+    {
+        String key = "AsEdFrTg7YhAsEdFrTg7YdFrTg7Yh994448559494855dSDFsdffsq";
+        String token = 
+            Jwts.builder()
+                    .setSubject(authResult.getName())
+                    .claim("authorities", authResult.getAuthorities())
+                    .setIssuedAt(new Date())
+                    .setExpiration(
+                        java.sql.Date.valueOf(
+                            LocalDate.now().plusWeeks(2)
+                        )
+                    )
+                    .signWith(Keys.hmacShaKeyFor(key.getBytes()))
+                    .compact();
+        
+        response.addHeader("Authorization", "Bearer " + token);
     }
 }
